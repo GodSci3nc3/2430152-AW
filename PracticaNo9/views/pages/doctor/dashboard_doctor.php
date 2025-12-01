1b6450d70ff169fe7ff4cfdad7dd58c48568899b
Arturo Rosales V <rosalesvelazquezarturo@gmail.com>
2025-11-30 23:46:02 -0600

    Timeline appointments view

--- PracticaNo9/views/pages/doctor/dashboard_doctor.php
+++ PracticaNo9/views/pages/doctor/dashboard_doctor.php
@@ -23,6 +23,7 @@ if(!isset($_SESSION['username'])){
     <link rel="icon" href="../../../resources/Medicore Logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="../../styles/globalStyles.css">
     <link rel="stylesheet" href="../../styles/dashboard.css">
 
@@ -70,27 +71,29 @@ if(!isset($_SESSION['username'])){
         <h1 class="text-primary-title">Agenda de hoy</h1>
         <hr>
 
-        <div class="timeline">
-            <div class="appointment-item">
-                <div class="hour">09:00</div>
-                <div class="info">María Gonzalez - Consulta general</div>
-            </div>
 
-            <div class="appointment-item">
-                <div class="hour">09:00</div>
-                <div class="info">María Gonzalez - Consulta general</div>
-            </div>
+<ol class="relative border-s border-default">                  
+    <li class="mb-10 ms-4">
+        <div class="absolute w-3 h-3 bg-neutral-quaternary rounded-full mt-1.5 -start-1.5 border border-buffer"></div>
+        <time class="text-sm font-normal leading-none text-body">February 2022</time>
+        <h3 class="text-lg font-semibold text-heading my-2">Application UI code in Tailwind CSS</h3>
+        <p class="mb-4 text-base font-normal text-body">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+    </li>
+    <li class="mb-10 ms-4">
+        <div class="absolute w-3 h-3 bg-neutral-quaternary rounded-full mt-1.5 -start-1.5 border border-buffer"></div>
+        <time class="text-sm font-normal leading-none text-body">March 2022</time>
+        <h3 class="text-lg font-semibold text-heading my-2">Marketing UI design in Figma</h3>
+        <p class="text-base font-normal text-body">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+    </li>
+    <li class="ms-4">
+        <div class="absolute w-3 h-3 bg-neutral-quaternary rounded-full mt-1.5 -start-1.5 border border-buffer"></div>
+        <time class="mb-1 text-sm font-normal leading-none text-body">April 2022</time>
+        <h3 class="text-lg font-semibold text-heading my-2">E-Commerce UI code in Tailwind CSS</h3>
+        <p class="text-base font-normal text-body">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
+    </li>
+</ol>
 
-            <div class="appointment-item">
-                <div class="hour">09:30</div>
-                <div class="info">María Gonzalez - Consulta general</div>
-            </div>
 
-            <div class="appointment-item">
-                <div class="hour">10:00</div>
-                <div class="info">María Gonzalez - Consulta general</div>
-            </div>
-        </div>
         </div>
 
         <div class="col">
@@ -111,6 +114,7 @@ if(!isset($_SESSION['username'])){
 
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
     <script src="../../js/dashboard_doctor.js"></script>
 </body>
 </html>
