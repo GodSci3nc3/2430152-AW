cb3a64a51c8333bf84f86dfab2d051469cb2b53d
Arturo Rosales V <rosalesvelazquezarturo@gmail.com>
2025-12-10 08:01:57 -0600

    Database add

--- examenRecu/eliminar_categoria.php
+++ examenRecu/eliminar_categoria.php
@@ -2,7 +2,7 @@
 require 'connection.php';
 
 if ($_POST) {
-    $stmt = $conn->prepare("DELETE FROM categorias WHERE id = ?");
+    $stmt = $conn->prepare("DELETE FROM  categorias WHERE id = ?");
     $stmt->execute([$_POST['id']]);
     header('Location: index.php');
     exit;
